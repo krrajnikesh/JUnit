accc850543460bb24156a00a90966d45a355e05f
krrajnikesh <krrajnikesh@gmail.com>
2025-01-13 22:27:30 +0530

    added parameterized csv file test

--- src/test/java/net/junit/parameterized/CsvSourceDemoTest.java
+++ src/test/java/net/junit/parameterized/CsvSourceDemoTest.java
@@ -2,6 +2,7 @@ package net.junit.parameterized;
 
 import net.junit.MathUtils;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +11,14 @@ public class CsvSourceDemoTest {
     @ParameterizedTest
     @CsvSource({"0, 1", "1, 1", "2, 2", "3, 6", "4, 24"})
     void factorialTest(int number, int expected){
+        MathUtils mathUtils = new MathUtils();
+
+        assertEquals(expected, mathUtils.factorial(number));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/factorial_data.csv")
+    void factorialTestUsingCsvFileSource(int number, int expected){
         MathUtils mathUtils = new MathUtils();
 
         assertEquals(expected, mathUtils.factorial(number));
